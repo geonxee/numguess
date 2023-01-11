d9090070b5fccba93278d9cb16b6404729ce69aa
geonxee <1913kg@naver.com>
2023-01-11 10:06:24 +0900

    feat: Count 'stay/change case'
    if changed door is winning door, add 1 to change.
    if stayed door is winning door, add 1 to change.

--- monty_hall.py
+++ monty_hall.py
@@ -4,3 +4,12 @@ doors = ['door1', 'door2', 'door3']
 
 winning_door = choice(doors)
 select_door = choice(doors)
+
+stay = 0
+change = 0
+
+if winning_door == select_door:
+    stay += 1
+else:
+    change += 1
+
