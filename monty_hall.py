65bc2af6f4ae15ef26ab3158a678c7f1c01926d5
geonxee <1913kg@naver.com>
2023-01-11 10:15:19 +0900

    feat: print simulation result
    calculate stay_rate and change_rate
    print stay_rate and change_rate

--- monty_hall.py
+++ monty_hall.py
@@ -2,14 +2,21 @@ from random import choice
 
 doors = ['door1', 'door2', 'door3']
 
-winning_door = choice(doors)
-select_door = choice(doors)
 
 stay = 0
 change = 0
 
-if winning_door == select_door:
-    stay += 1
-else:
-    change += 1
+for i in range(10000):
+    winning_door = choice(doors)
+    select_door = choice(doors)
+        
+    if winning_door == select_door:
+        stay += 1
+    else:
+        change += 1
 
+stay_rate = round((stay/10000) * 100,2)
+change_rate = round((change/10000) * 100,2)
+
+print(f'10000 times of simulation: {change} times win({change_rate}% change) {stay} times win({stay_rate}% stay)')
+    
